b2c3820a7b2cfd15b17eddaaac6c034808a4fa7b
Manuela Roque <65978895+manuelaroquee@users.noreply.github.com>
2023-11-12 12:51:08 -0400

    Create exibirMensagem.c

---
+++ exibirMensagem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Procedimento para exibir "Bom dia" na tela
+void mensagem() {
+    printf("Bom dia\n");
+}
+
+int main() {
+    // Chamar o procedimento para exibir a mensagem
+    mensagem();
+
+    return 0;
+}
