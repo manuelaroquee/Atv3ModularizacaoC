0bd1246c391a97eeda1381fea84a631d1dd175be
Manuela Roque <65978895+manuelaroquee@users.noreply.github.com>
2023-11-12 14:39:15 -0400

    Create alg02MaiorNumero.c

---
+++ alg02MaiorNumero.c
@@ -0,0 +1,23 @@
+  #include <stdio.h>
+
+#include <stdio.h>
+int funcaoretorna(float a, float b){
+    if (a>b){
+        return a;
+    }else if (b>a){
+        return b;
+    }else{
+        printf("os dois valores sao iguais");
+    }
+}
+
+int main(){
+    int n1, n2;
+    printf("Informe o primeiro valor = ");
+    scanf("%d", &n1);
+    printf("Informe o segundo valor = ");
+    scanf("%d", &n2);
+    printf("Maior valor = %d", funcaoretorna(n1, n2));
+    
+    return 0;
+  }
